3d80be197bcb42191ce0d47b4c3fdcd3ff34239b
Pietro Attardi <peter@Pietros-MacBook-Air.local>
2022-01-24 15:55:55 +0100

    heroku files

--- familudget/settings.py
+++ familudget/settings.py
@@ -144,5 +144,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://localhost:8000",
-    "http://127.0.0.1:8000"
+    "http://127.0.0.1:8000",
+    'https://familudget.herokuapp.com',
+    'https://familudget.herokuapp.com/',
 ]
