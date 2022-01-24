35185c95a1c39cbec3a2628dd30ca017f0893ee9
Pietro Attardi <peter@Pietros-MacBook-Air.local>
2022-01-24 15:37:59 +0100

    heroku files

--- familudget/urls.py
+++ familudget/urls.py
@@ -5,5 +5,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('familudget_api.urls')),
-    path('/',TemplateView.as_view(template_name='index.html'))
+    path('',TemplateView.as_view(template_name='index.html'))
 ]
