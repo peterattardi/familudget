731d0199d679e6f5804ccf25adc3706067d019b9
Pietro Attardi <peter@Pietros-MacBook-Air.local>
2022-01-27 15:08:08 +0100

    cors

--- familudget/settings.py
+++ familudget/settings.py
@@ -154,8 +154,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    # 'https://familudget.herokuapp.com',
-    # 'https://familudget.herokuapp.com/',
+    'https://familudget.herokuapp.com',
+    'https://familudget.herokuapp.com/',
 ]
 
 SIMPLE_JWT = {
